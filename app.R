8db3edfd7cc721829a7fa202a60aac99d9e66125
Joseph Yun <josephyun2@gmail.com>
2019-03-09 16:01:20 -0800

    finish bar chart for q1

--- app.R
+++ app.R
@@ -4,57 +4,38 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 
-
+# read data files into data frames and clean them up
 netflix_orig <- read.csv("netflix.csv", stringsAsFactors = FALSE)
 netflix_shows <- read.csv("netflix_shows.csv", stringsAsFactors = FALSE) %>% 
   distinct() %>% 
-  head(109) %>% 
-  mutate(group = "netflix")
+  head(108) %>% 
+  mutate(group = "Netflix")
 hulu_shows <- read.csv("hulu.csv", stringsAsFactors = FALSE) %>% 
   select(major_genre:release_data) %>% 
-  distinct() %>% 
-  mutate(group = "hulu")
+  mutate(group = "Hulu")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+# ui goes here
+
+
+
+
 # Question 1: which age rating group is more prevalent in each platform?
 #             static bar graph with each platform and then bars with age ratings
 
-
 both_shows <- full_join(netflix_shows, hulu_shows)
-
-ggplot(both_shows, aes(rating)) +
-  geom_bar(aes(fill = group),
-           position = "dodge"
-) +
-  labs(title = "Number of Shows by MPAA Rating",
-       x = "MPAA Rating",
-       y = "Number of Shows",
-       fill = "Platform")
-  
 
 server <- function(input, output) {
    
+  # static bar chart for age rating shows
+  ggplot(both_shows, aes(rating)) +
+    geom_bar(aes(fill = group),
+             position = "dodge") +
+    scale_fill_manual("Platform", 
+                      values = c("Hulu" = "#09e41f", "Netflix" = "#fd4040")) +
+    labs(title = "Number of Shows by MPAA Rating",
+         x = "MPAA Rating",
+         y = "Number of Shows",
+         fill = "Platform")
    
 }
 
